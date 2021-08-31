85916366fd2eb702fd8331b6e01c7597f7605e95
drewelewis <29957923+drewelewis@users.noreply.github.com>
2021-08-30 20:18:42 -0400

    Add project files.

---
+++ AzureFunctionsWithRedis/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionsWithRedis
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string employeeId, string name, int age)
+        {
+            Id = employeeId;
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ AzureFunctionsWithRedis/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AzureFunctionsWithRedis
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddStackExchangeRedisCache((Action<RedisCacheOptions>)(options => options.Configuration = Environment.GetEnvironmentVariable("RedisConnection")));
+            builder.Services.Add(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
+        }
+    }
+}
